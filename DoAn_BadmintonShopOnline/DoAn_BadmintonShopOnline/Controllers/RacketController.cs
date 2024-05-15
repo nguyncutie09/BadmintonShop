a3e639c4c78a42ec857a65d720cd7f55fd7898ee
nguyncutie09 <lethanhkynguyen2k3@gmail.com>
2024-05-15 19:50:31 +0700

    Upload Project first time

---
+++ DoAn_BadmintonShopOnline/DoAn_BadmintonShopOnline/Controllers/RacketController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DoAn_BadmintonShopOnline.Controllers;
+
+namespace DoAn_BadmintonShopOnline.Controllers
+{
+    public class RacketController : Controller
+    {
+        // GET: Shop
+        public ActionResult Yonex()
+        {
+            return View();
+        }
+
+        public ActionResult YonexRacket1()
+        {
+            return View();
+        }
+    }
+}
